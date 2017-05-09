ebc7cd6d22679d691f9705c8998e20408b668ea0
KouyaHirata <atarih.ayuok@gmail.com>
2017-05-09 12:54:08 +0000

    edit conroller and create view

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,17 @@
 class BlogsController < ApplicationController
+  
   def index
   end
+  
+  
+  def new
+    # この変数は View でも使える
+    @blog = Blog.new
+  end
+  
+  
+  def create
+    
+  end
+  
 end
