5c3c96575e4013e785b23564c016ccb1cd3b9858
KouyaHirata <atarih.ayuok@gmail.com>
2017-05-12 11:35:10 +0000

    add contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,12 +1,25 @@
 class ContactsController < ApplicationController
     
     def new
-        
+        @contact = Contact.new
     end
     
     
     def create
-        
+        Contact.create(contacts_params)
+        redirect_to new_contact_url
+    end
+    
+    
+    
+    
+    
+    ########
+    private
+    ########
+    
+    def contacts_params
+       params.require(:contact).permit(:name, :email, :content) 
     end
     
 end
