1ff9b8368d14632b04476fc1df4c2a702183988c
KouyaHirata <atarih.ayuok@gmail.com>
2017-05-13 12:39:46 +0000

    add delete.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :blogs, only: [:index, :new, :create, :edit, :update]
+  resources :blogs, only: [:index, :new, :create, :edit, :update, :destroy]
   
   resources :contacts, only: [:new, :create]
   
