e342a2e1cfc7988ab09a30ac988261afc195ecf1
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-13 09:14:59 +0900

    add image to blogs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,14 @@
 module ApplicationHelper
+  require "mini_magick"
+
   def profile_img(user)
-    return image_tag(user.avatar, alt: user.name) if user.avatar?
-    
     unless user.provider.blank?
       img_url = user.image_url
     else
       img_url = 'no_image.png'
     end
-    image_tag(img_url, alt: user.name)
+
+    #return
+    image_tag(img_url, alt: user.name, :size => "50x50")
   end
 end
