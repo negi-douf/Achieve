db2913eec7d12be74d420a1f3287b7f7b7d284b5
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-08-27 12:33:12 +0900

    add js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,6 +8,7 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.js { render :index }
       else
         format.html { render :new }
       end
