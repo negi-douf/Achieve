595ccbb0b88881a52dac60d5f19b48e39eb81ca0
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-09-08 17:48:13 +0900

    padding

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,10 +3,6 @@ class Comment < ActiveRecord::Base
   belongs_to :blog
 
   def pusher_trigger(current_user)
-    unless self.blog.user.id == current_user.id
-      Pusher.trigger('user_#{self.blog.user_id}_channel', 'comment_created', {
-        message: 'あなたの作成したブログにコメントが投稿されました'
-      })
-    end
+    
   end
 end
