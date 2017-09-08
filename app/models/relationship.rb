7975655ab11c2d0d8e8d1897dfc843db5bc0b442
Hirata Koya <hiratakoya@HiratanoMacBook-Pro.local>
2017-09-08 15:09:39 +0900

    add validation to  relationships

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,6 @@
 class Relationship < ActiveRecord::Base
+  validates :follower_id, :uniqueness => {:scope => :followed_id}
+
   # follower という名前の参照関係を定義
   # User に属する
   belongs_to :follower, class_name: "User"
